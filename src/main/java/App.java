2d23548b52ef8daf0c87f90f3669b3b18139c32c
Kanish-Kumar-S <kanishkumar.s2023@vitstudent.ac.in>
2026-04-13 10:05:40 +0530

    p

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,16 @@
 public class App {
- public static int add(int a, int b) {
- return a + b;
+ public static int otp(int sent, int recieved) {
+    if(sent==recieved)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+ 
  }
  public static void main(String[] args) {
- System.out.println("Hello DevOps!");
+    System.out.println(otp(2,3));
  }
 }
