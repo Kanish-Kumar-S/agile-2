f4fa62ccc364c916ca6c6f063878930666fc9ab1
Kanish-Kumar-S <kanishkumar.s2023@vitstudent.ac.in>
2026-04-13 09:32:13 +0530

    g

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,43 +1,8 @@
-import java.util.*;
-
-class Transaction {
-    String date;
-    String type; // CREDIT / DEBIT
-    double amount;
-    double balance;
-
-    public Transaction(String date, String type, double amount, double balance) {
-        this.date = date;
-        this.type = type;
-        this.amount = amount;
-        this.balance = balance;
-    }
-}
-
 public class App {
-
-    public static List<String> generateStatement(List<Transaction> transactions) {
-        List<String> result = new ArrayList<>();
-
-        result.add("DATE | TYPE | AMOUNT | BALANCE");
-
-        for (Transaction t : transactions) {
-            result.add(t.date + " | " + t.type + " | " + t.amount + " | " + t.balance);
-        }
-
-        return result;
-    }
-
-    // optional main for manual run
-    public static void main(String[] args) {
-        List<Transaction> txns = new ArrayList<>();
-        txns.add(new Transaction("2026-04-01", "CREDIT", 1000, 1000));
-        txns.add(new Transaction("2026-04-02", "DEBIT", 200, 800));
-
-        List<String> statement = generateStatement(txns);
-
-        for (String line : statement) {
-            System.out.println(line);
-        }
-    }
+ public static int add(int a, int b) {
+ return a + b;
+ }
+ public static void main(String[] args) {
+ System.out.println("Hello DevOps!");
+ }
 }
